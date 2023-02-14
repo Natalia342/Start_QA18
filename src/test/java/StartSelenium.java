eb25da354dc1f11bdea9cac76260f572d3bfc68f
Natalia342 <nataliatalova7@gmail.com>
2023-02-14 21:02:47 +0200

    Methodes

---
+++ src/test/java/StartSelenium.java
@@ -0,0 +1,22 @@
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class StartSelenium {
+    WebDriver wd;//это поле (его нужно инецелизировать)
+
+    @BeforeMethod
+    public void preCondition(){
+
+    }
+    @Test
+    public void testLogin(){
+
+    }
+    @AfterMethod
+    public void postCondition(){
+
+    }
+
+}
