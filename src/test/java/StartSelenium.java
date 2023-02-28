a7b49559030802d1309394c82fcf1bec8fac4e27
Natalia342 <nataliatalova7@gmail.com>
2023-02-28 21:03:48 +0200

    findElement

--- src/test/java/StartSelenium.java
+++ src/test/java/StartSelenium.java
@@ -16,15 +16,28 @@ public class StartSelenium {
         System.out.println("Opening web-site");
         wd = new ChromeDriver();
         //   wd.get("https://telranedu.web.app/home");//go wed-site только 1 раз
-        wd.navigate().to("https://telranedu.web.app/home");
-
+       //    wd.navigate().to("https://telranedu.web.app/home");
+        wd.navigate().to("https://telranedu.web.app/login");
     }
 
     @Test
     public void testLogin() {
-        WebElement element = wd.findElement(By.tagName("a"));
+    /*    WebElement element = wd.findElement(By.tagName("a"));
+        List<WebElement> list = wd.findElements(By.tagName("a"));//dженерик
+        System.out.println(list.size());*/
+        WebElement form = wd.findElement(By.tagName("form"));
+        System.out.println(form);
+        WebElement root = wd.findElement(By.id("root"));
+
+        wd.findElement(By.className("navbar-component_nav__1X_4m"));
+        wd.findElement(By.linkText("ABOUT"));
+        wd.findElement(By.name("password"));
+        wd.findElement(By.partialLinkText("LOG"));
+
+
         List<WebElement> list = wd.findElements(By.tagName("a"));//dженерик
         System.out.println(list.size());
+
     }
 
     @AfterMethod
